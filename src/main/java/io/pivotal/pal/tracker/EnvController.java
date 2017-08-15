8aef3bba4635cfcfc1e06a78adbf733d0f19cbef
Katie Moulang <kmoulang@pivotal.io>
2017-08-15 11:02:37 +0100

    Pull in env variables

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddress;
+
+    public EnvController(
+       @Value("${port:NOT SET}") String port,
+       @Value("${memory_limit:NOT SET}") String memoryLimit,
+       @Value("${cf_instance_index:NOT SET}") String cfInstanceIndex,
+       @Value("${cf_instance_addr:NOT SET}") String cfInstanceAddress
+    ) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+        return env;
+    }
+}
