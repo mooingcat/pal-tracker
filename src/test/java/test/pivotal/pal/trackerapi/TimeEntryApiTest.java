8116f09c00d28c27e33bb481700a513c7f13c548
Katie Moulang <kmoulang@pivotal.io>
2017-08-18 14:18:09 +0200

    Enable spring security

--- src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
@@ -8,8 +8,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -28,8 +30,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
 public class TimeEntryApiTest {
 
-    @Autowired
+    @LocalServerPort
+    private String port;
     private TestRestTemplate restTemplate;
+
+    @Before
+    public void setup() {
+        RestTemplateBuilder builder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + port)
+                .basicAuthorization("user", "password");
+        restTemplate = new TestRestTemplate(builder);
+    }
 
     private TimeEntry timeEntry = new TimeEntry(123, 456, "today", 8);
 
